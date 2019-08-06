6c61db449d037b340ff6c82875f3dd7aa7e81187
Divya Sharma <divyash@codeaurora.org>
2019-08-05 18:27:37 -0700

    Add patches to allow removal of BUILD_BROKEN_PHONY_TARGETS=true
    Allowing broken phony targets to build has been removed, which means that
    setting BUILD_BROKEN_PHONY_TARGETS=true has no effect.
    
    Change-Id: Idfa35fd24aaf0368f27853b82f9308423368c6c8

--- build/QSSI_violators.mk
+++ build/QSSI_violators.mk
@@ -1,10 +1,15 @@
 # Copyright (c) 2018, The Linux Foundation. All rights reserved.
 
+.PHONY: qssi_violators
 qssi_violators: $(PRODUCT_OUT)/module-info.json
 # Remove existing QSSI violators list (if present)
 	if [ -s $$OUT/QSSI_violators.txt ]; then rm $$OUT/QSSI_violators.txt; fi
 # Generate QSSI violators list
 	vendor/qcom/opensource/core-utils/build/QSSI_violators
+
+# KEYSTONE(I87be6f43b8940acf227798d99500af4a22551cbc,b/117238422)
+
+# Remove unused targets below, and also mark phony targets as phony.
 
 # module-info.json is not included when ONE_SHOT_MAKEFILE,
 # hence disable qssi_violators for that as well.
@@ -13,8 +18,18 @@ ifndef ONE_SHOT_MAKEFILE
   ifdef PRODUCT_SHIPPING_API_LEVEL
     ifneq ($(call math_gt_or_eq,$(PRODUCT_SHIPPING_API_LEVEL),28),)
       droidcore: qssi_violators
+      # droidcore_system appears to be obsolete.
+      # droidcore_system: qssi_violators
+      # droidcore_non_system should probably be obsolete, but it's still in use.
+      .PHONY: droidcore_non_system
+      droidcore_non_system: qssi_violators
     endif
   else # PRODUCT_SHIPPING_API_LEVEL is undefined
     droidcore: qssi_violators
+    # droidcore_system appears to be obsolete.
+    # droidcore_system: qssi_violators
+    # droidcore_non_system should probably be obsolete, but it's still in use.
+    .PHONY: droidcore_non_system
+    droidcore_non_system: qssi_violators
   endif
 endif
