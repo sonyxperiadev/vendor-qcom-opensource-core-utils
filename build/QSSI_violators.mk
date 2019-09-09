38e972e69b3f2b63c372f48efcf9ebf66bf05b7f
padarshr <padarshr@codeaurora.org>
2019-09-09 12:20:13 +0530

    Remove the un-used droidcore_non_system/droidcore_system.
    As we no longer use droidcore_non_system/droidcore_system,
    cleaning up the same.
    
    Change-Id: Iad28f51ae0510ca55934825991cd92b32a7a5201

--- build/QSSI_violators.mk
+++ build/QSSI_violators.mk
@@ -7,10 +7,6 @@ qssi_violators: $(PRODUCT_OUT)/module-info.json
 # Generate QSSI violators list
 	vendor/qcom/opensource/core-utils/build/QSSI_violators
 
-# KEYSTONE(I87be6f43b8940acf227798d99500af4a22551cbc,b/117238422)
-
-# Remove unused targets below, and also mark phony targets as phony.
-
 # module-info.json is not included when ONE_SHOT_MAKEFILE,
 # hence disable qssi_violators for that as well.
 # Also, QSSI enforcement is needed only for android-P(and above) new-launch devices.
@@ -18,18 +14,8 @@ ifndef ONE_SHOT_MAKEFILE
   ifdef PRODUCT_SHIPPING_API_LEVEL
     ifneq ($(call math_gt_or_eq,$(PRODUCT_SHIPPING_API_LEVEL),28),)
       droidcore: qssi_violators
-      # droidcore_system appears to be obsolete.
-      # droidcore_system: qssi_violators
-      # droidcore_non_system should probably be obsolete, but it's still in use.
-      .PHONY: droidcore_non_system
-      droidcore_non_system: qssi_violators
     endif
   else # PRODUCT_SHIPPING_API_LEVEL is undefined
     droidcore: qssi_violators
-    # droidcore_system appears to be obsolete.
-    # droidcore_system: qssi_violators
-    # droidcore_non_system should probably be obsolete, but it's still in use.
-    .PHONY: droidcore_non_system
-    droidcore_non_system: qssi_violators
   endif
 endif
