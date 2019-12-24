e0b06d4e514fb123e5352389496f3ce8bec1b174
Tapas Pradhan <tpradhan@codeaurora.org>
2019-12-24 15:58:13 +0530

    Add support for bengal_32go and qssi_32go to build.sh.
    Add bengal_32go to the QSSI target list.
    QSSI needs to compile with qssi_32go lunch
    option instead of qssi.
    
    Change-Id: If285bca794e5e471bd2c9319e6a6db76a9b1f8bf

--- build/build.sh
+++ build/build.sh
@@ -140,12 +140,15 @@ if [[ "$MERGE_ONLY" == 1 ]]; then
     fi
 fi
 
-QSSI_TARGETS_LIST=("lahaina" "sdm710" "sdm845" "msmnile" "sm6150" "kona" "atoll" "trinket" "trinket_32" "lito" "bengal" "bengal_32")
+QSSI_TARGETS_LIST=("lahaina" "sdm710" "sdm845" "msmnile" "sm6150" "kona" "atoll" "trinket" "trinket_32" "lito" "bengal" "bengal_32" "bengal_32go")
 QSSI_TARGET_FLAG=0
 
 case "$TARGET_PRODUCT" in
     *_32)
         TARGET_QSSI="qssi_32"
+        ;;
+    *_32go)
+        TARGET_QSSI="qssi_32go"
         ;;
     *)
         TARGET_QSSI="qssi"
@@ -175,7 +178,7 @@ QSSI_ARGS_WITHOUT_DIST=""
 DIST_DIR="out/dist"
 MERGED_TARGET_FILES="$DIST_DIR/merged-qssi_${TARGET_PRODUCT}-target_files.zip"
 MERGED_OTA_ZIP="$DIST_DIR/merged-qssi_${TARGET_PRODUCT}-ota.zip"
-DIST_ENABLED_TARGET_LIST=("lahaina" "kona" "sdm710" "sdm845" "msmnile" "sm6150" "trinket" "trinket_32" "lito" "bengal" "bengal_32" "atoll")
+DIST_ENABLED_TARGET_LIST=("lahaina" "kona" "sdm710" "sdm845" "msmnile" "sm6150" "trinket" "trinket_32" "lito" "bengal" "bengal_32" "atoll" "bengal_32go")
 DYNAMIC_PARTITION_ENABLED_TARGET_LIST=("lahaina" "kona" "msmnile" "sdm710" "lito" "trinket" "trinket_32" "atoll" "bengal" "bengal_32")
 DYNAMIC_PARTITIONS_IMAGES_PATH=$OUT
 DP_IMAGES_OVERRIDE=false
@@ -200,7 +203,7 @@ QSSI_ARGS="$QSSI_ARGS BOARD_DYNAMIC_PARTITION_ENABLE=$BOARD_DYNAMIC_PARTITION_EN
 # Set Shipping API level on target basis.
 SHIPPING_API_P="28"
 SHIPPING_API_Q="29"
-SHIPPING_API_P_TARGET_LIST=("sdm845" "sm6150")
+SHIPPING_API_P_TARGET_LIST=("sdm845" "sm6150" "bengal_32go")
 SHIPPING_API_LEVEL=$SHIPPING_API_Q
 for P_API_TARGET in "${SHIPPING_API_P_TARGET_LIST[@]}"
 do
