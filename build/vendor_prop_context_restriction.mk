ca0c655e16355d01400a0ae75cec43f88b955099
Mulugeta Engdaw <mengdaw@codeaurora.org>
2020-01-30 11:07:14 -0800

    Enforce vendor property/context naming restriction
        - switch to enforcing mode from warning mode
        - minor cleanup
    
    Change-Id: Id7f17eb02dee1603c5f44c8771660027d29fe2fd

--- build/vendor_prop_context_restriction.mk
+++ build/vendor_prop_context_restriction.mk
@@ -2,7 +2,7 @@
 .PHONY:vendor_prop_context_restriction_enforcement
 
 vendor_prop_context_restriction_enforcement:
-	python vendor/qcom/opensource/core-utils/build/vendor_prop_context_restriction.py --m warning
+	python vendor/qcom/opensource/core-utils/build/vendor_prop_context_restriction.py --m error
 
 ifneq ($(TARGET_PRODUCT), qssi)
 droidcore:vendor_prop_context_restriction_enforcement
