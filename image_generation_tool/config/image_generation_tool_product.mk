0fbdf7a56b0310138a936f2a091ab0ea5714a770
Roopesh Nataraja <roopeshr@codeaurora.org>
2021-01-22 17:35:28 -0800

    core-utils: Enable compilation of image_gen_tool_test
    image_gen_tool_test is a test framework to validate
    image_generation_tool. Enable compilation of this
    python host test binary.
    
    Change-Id: I5165e2a691e3676e819a5ba95f26750f8c271b49

--- image_generation_tool/config/image_generation_tool_product.mk
+++ image_generation_tool/config/image_generation_tool_product.mk
@@ -1,4 +1,5 @@
 ifeq ($(TARGET_USES_IMAGE_GEN_TOOL),true)
 IMAGE_GENERATION_TOOLS := image_generation_tool
+IMAGE_GENERATION_TOOLS += image_gen_tool_test
 PRODUCT_HOST_PACKAGES += $(IMAGE_GENERATION_TOOLS)
 endif
