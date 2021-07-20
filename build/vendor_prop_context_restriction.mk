c90c91e21a4f153d1569bbb8a7cc9417d056f562
Huang Li <lhuang@codeaurora.org>
2021-07-20 16:53:07 +0800

    core-utils: Replace usage of TARGET_PRODUCT with TARGET_BOARD_PLATFORM
    For vendor_prop_context_restriction.mk
    
    TARGET_PRODUCT is supposed to be used by OEMs not Chipset vendors,
    So use TARGET_BOARD_PLATFORM instead of TARGET_PRODUCT.
    
    Change-Id: I226bd330d00a7624a539b359ccf835d19e3d663d
    CRs-Fixed: 2979907

--- build/vendor_prop_context_restriction.mk
+++ build/vendor_prop_context_restriction.mk
@@ -4,7 +4,7 @@
 vendor_prop_context_restriction_enforcement:
 	python vendor/qcom/opensource/core-utils/build/vendor_prop_context_restriction.py --m error
 
-ifneq ($(TARGET_PRODUCT), qssi)
+ifneq ($(TARGET_BOARD_PLATFORM), qssi)
 droidcore:vendor_prop_context_restriction_enforcement
 droidcore-unbundled:vendor_prop_context_restriction_enforcement
 endif
